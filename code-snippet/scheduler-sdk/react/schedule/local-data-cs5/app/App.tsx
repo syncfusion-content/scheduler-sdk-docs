d1e1a959f32f79477dae8fee12d2a71b1af01e20
Sivakumar Manimaran <sivakumar.manimaran@syncfusion.com>
2026-08-14 11:48:07 +0530

    1045314: Enhancement of React UG documents.

---
+++ code-snippet/scheduler-sdk/react/schedule/local-data-cs5/app/App.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, Inject, EventSettingsModel } from '@syncfusion/ej2-react-schedule';
+import { DataManager, ODataV4Adaptor, Query } from '@syncfusion/ej2-data';
+
+function App() {
+  const [dataManager, setDataManager] = React.useState<DataManager | null>(null);
+
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const manager = new DataManager({
+        url: 'https://services.odata.org/V4/Northwind/Northwind.svc/Orders/',
+        adaptor: new ODataV4Adaptor()
+      });
+      await manager.ready;
+      const query = new Query().addParams('readOnly', 'true');
+      const data = await manager.executeQuery(query) as any;
+      setDataManager(manager);    
+    };
+
+    fetchData();
+  }, []);
+  const fieldsData = {
+    id: 'Id',
+    subject: { name: 'ShipName' },
+    location: { name: 'ShipCountry' },
+    description: { name: 'ShipAddress' },
+    startTime: { name: 'OrderDate' },
+    endTime: { name: 'RequiredDate' },
+    recurrenceRule: { name: 'ShipRegion' }
+  }
+  const eventSettings: EventSettingsModel = { dataSource: dataManager, fields: fieldsData };
+
+  return (
+    <ScheduleComponent
+      height='550px'
+      readonly={true}
+      eventSettings={eventSettings}
+      selectedDate={new Date(1996, 6, 9)}
+    >
+      <Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+    </ScheduleComponent>
+  );
+}
+
+export default App;
